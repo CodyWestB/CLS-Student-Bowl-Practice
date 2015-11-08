15c5c27a4a7288226cc99bccb0a2003447f6854f
Cody <codywestb@gmail.com>
2015-11-08 14:31:36 -0600

    Created Project
    This is the first initial creation of the GitHub project for Chase's CLS
    Student Bowl Practice solution. This is Checkpoint 0.

---
+++ CLS Student Bowl Practice/GameSelect.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CLS_Student_Bowl_Practice
+{
+    /// <summary>
+    /// Interaction logic for GameSelect.xaml
+    /// </summary>
+    public partial class GameSelect : Window
+    {
+        private MainWindow game = null;
+
+        public GameSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void gameSelect_onLoad(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.type == 1)
+            {
+                lblType.Content = "MLS";
+            }
+
+            if (MainWindow.type == 2)
+            {
+                lblType.Content = "MLT";
+            }
+
+            if (MainWindow.year == 1)
+            {
+                lblYear.Content = "Year A";
+            }
+
+            if (MainWindow.year == 2)
+            {
+                lblYear.Content = "Year B";
+            }
+
+            if (MainWindow.year == 3)
+            {
+                lblYear.Content = "Year C";
+            }
+
+            if (MainWindow.year == 4)
+            {
+                lblYear.Content = "Year D";
+            }
+
+
+        }
+
+        internal MainWindow getNewGame()
+        {
+            this.ShowDialog();
+            return game;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CLS Student Bowl Practice/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CLS_Student_Bowl_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int year;
+        public static int type;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainWindow_onLoad(object sender, RoutedEventArgs e)
+        {
+            cmbYear.Items.Add("Year A");
+            cmbYear.Items.Add("Year B");
+            cmbYear.Items.Add("Year C");
+            cmbYear.Items.Add("Year D");
+        }
+
+        private void cmbYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbYear.SelectedItem.ToString() == "Year A")
+            {
+                year = 1;
+            }
+
+            if (cmbYear.SelectedItem.ToString() == "Year B")
+            {
+                year = 2;
+            }
+
+            if (cmbYear.SelectedItem.ToString() == "Year C")
+            {
+                year = 3;
+            }
+
+            if (cmbYear.SelectedItem.ToString() == "Year D")
+            {
+                year = 4;
+            }
+        }
+
+        private void btnMLS_Click(object sender, RoutedEventArgs e)
+        {
+            type = 1;
+
+            this.Visibility = Visibility.Collapsed;
+            GameSelect newGame = new GameSelect();
+            MainWindow game = newGame.getNewGame();
+            this.Visibility = Visibility.Visible;
+            this.ShowDialog();
+        }
+
+        private void btnMLT_Click(object sender, RoutedEventArgs e)
+        {
+            type = 2;
+
+            this.Visibility = Visibility.Collapsed;
+            GameSelect newGame = new GameSelect();
+            MainWindow game = newGame.getNewGame();
+            this.Visibility = Visibility.Visible;
+            this.ShowDialog();
+        }
+    }
+}
