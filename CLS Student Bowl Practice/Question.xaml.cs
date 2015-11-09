a09e1cdd896ae2d30b3433d6a441800c5c0bfaad
jaimech <chase.h.jaime@gmail.com>
2015-11-08 18:30:09 -0600

    Added functionality to Questions

--- CLS Student Bowl Practice/Question.xaml.cs
+++ CLS Student Bowl Practice/Question.xaml.cs
@@ -27,6 +27,154 @@ namespace CLS_Student_Bowl_Practice
             InitializeComponent();
         }
 
+        private void question_onLoad(object sender, RoutedEventArgs e)
+        {
+
+            if (QuestionSelect.categoryNumber == 1)
+            {
+                lblCategory.Content = "Blood Bank";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 2)
+            {
+                lblCategory.Content = "Body Fluids";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 3)
+            {
+                lblCategory.Content = "Chemistry";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 4)
+            {
+                lblCategory.Content = "Coagulation";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 5)
+            {
+                lblCategory.Content = "Hematology";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 6)
+            {
+                lblCategory.Content = "Immunology";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 7)
+            {
+                lblCategory.Content = "Lab Operations";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 8)
+            {
+                lblCategory.Content = "Micro/Parasit/Mycol";
+                lblAmount.Content = "5";
+            }
+
+            if (QuestionSelect.categoryNumber == 9)
+            {
+                lblCategory.Content = "Blood Bank";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 10)
+            {
+                lblCategory.Content = "Body Fluids";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 11)
+            {
+                lblCategory.Content = "Chemistry";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 12)
+            {
+                lblCategory.Content = "Coagulation";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 13)
+            {
+                lblCategory.Content = "Hematology";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 14)
+            {
+                lblCategory.Content = "Immunology";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 15)
+            {
+                lblCategory.Content = "Lab Operations";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 16)
+            {
+                lblCategory.Content = "Micro/Parasit/Mycol";
+                lblAmount.Content = "10";
+            }
+
+            if (QuestionSelect.categoryNumber == 17)
+            {
+                lblCategory.Content = "Blood Bank";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 18)
+            {
+                lblCategory.Content = "Body Fluids";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 19)
+            {
+                lblCategory.Content = "Chemistry";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 20)
+            {
+                lblCategory.Content = "Coagulation";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 21)
+            {
+                lblCategory.Content = "Hematology";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 22)
+            {
+                lblCategory.Content = "Immunology";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 23)
+            {
+                lblCategory.Content = "Lab Operations";
+                lblAmount.Content = "20";
+            }
+
+            if (QuestionSelect.categoryNumber == 24)
+            {
+                lblCategory.Content = "Micro/Parasit/Mycol";
+                lblAmount.Content = "20";
+            }
+        }
+
         internal QuestionSelect getNewCategory()
         {
             this.ShowDialog();
